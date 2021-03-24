2fde53c04dafec0179cc20b2373ea54a51c32098
daiwenzh5 <daiwenzh5@163.com>
2021-03-25 01:29:17 +0800

    Site updated: 2021-03-25 01:29:17

--- sw-register.js
+++ sw-register.js
@@ -1,1 +1,1 @@
-navigator.serviceWorker&&navigator.serviceWorker.register('/sw.js?v=20210325011053').then(function(){navigator.serviceWorker.addEventListener('message',function(a){'sw.update'===a.data&&$.message({title:'更新成功',message:'',type:'success'});})});
+navigator.serviceWorker&&navigator.serviceWorker.register('/sw.js?v=20210325012903').then(function(){navigator.serviceWorker.addEventListener('message',function(a){'sw.update'===a.data&&$.message({title:'更新成功',message:'',type:'success'});})});
