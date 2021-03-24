96aa0d5a412fe961a55f01d7ed502458e88e907b
DaiwenZh5 <DaiwenZh5@users.noreply.github.com>
2021-03-25 02:13:31 +0800

    添加钉钉消息 DaiwenZh5/hexo@3400d4903b3ff86a88ac07b9ac74ffd449e973db

--- sw-register.js
+++ sw-register.js
@@ -1,1 +1,1 @@
-navigator.serviceWorker&&navigator.serviceWorker.register('/sw.js?v=20210325015252').then(function(){navigator.serviceWorker.addEventListener('message',function(a){'sw.update'===a.data&&$.message({title:'更新成功',message:'',type:'success'});})});
+navigator.serviceWorker&&navigator.serviceWorker.register('/sw.js?v=20210325021328').then(function(){navigator.serviceWorker.addEventListener('message',function(a){'sw.update'===a.data&&$.message({title:'更新成功',message:'',type:'success'});})});
