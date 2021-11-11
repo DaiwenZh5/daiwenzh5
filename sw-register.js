e841b54e6838c4a92c31830ccb759d234c3b74ea
DaiwenZh5 <DaiwenZh5@users.noreply.github.com>
2021-11-11 10:38:07 +0800

    deploy: DaiwenZh5/hexo@88d99055fe9cedbf3ae95d39d75db581a17326bd

--- sw-register.js
+++ sw-register.js
@@ -1,1 +1,1 @@
-navigator.serviceWorker&&navigator.serviceWorker.register('/sw.js?v=20211110195415').then(function(){navigator.serviceWorker.addEventListener('message',function(a){'sw.update'===a.data&&$.message({title:'更新成功',message:'',type:'success'});})});
+navigator.serviceWorker&&navigator.serviceWorker.register('/sw.js?v=20211111103805').then(function(){navigator.serviceWorker.addEventListener('message',function(a){'sw.update'===a.data&&$.message({title:'更新成功',message:'',type:'success'});})});
